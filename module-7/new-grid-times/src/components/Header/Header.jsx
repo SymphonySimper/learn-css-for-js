1540eb517f5e2611468310350018db58aa3fe56f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-04-05 15:13:46 +0530

    feat(module-7/new-grid-times): complete task 1

--- module-7/new-grid-times/src/components/Header/Header.jsx
+++ module-7/new-grid-times/src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -21,11 +21,18 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
-          <ActionGroup>
+          <DesktopHeader>
+            <Logo />
+          </DesktopHeader>
+          <MobileActionGroup>
             <button>
               <User size={24} />
             </button>
-          </ActionGroup>
+          </MobileActionGroup>
+          <DesktopActionGroup>
+            <Button>Subscribe</Button>
+            <a>Already a subscriber?</a>
+          </DesktopActionGroup>
         </Row>
       </SuperHeader>
       <MainHeader>
@@ -39,11 +46,22 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    background: var(--color-gray-100);
+    color: var(--color-offblack);
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    place-content: center;
+  }
 `;
 
 const ActionGroup = styled.div`
@@ -59,12 +77,48 @@ const ActionGroup = styled.div`
   }
 `;
 
+const MobileActionGroup = styled(ActionGroup)`
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopActionGroup = styled.div`
+  display: none;
+  flex-direction: column;
+  gap: 8px;
+  align-self: end;
+  justify-self: end;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+
+  a {
+    font-family: var(--font-family-san-serif);
+    text-decoration: underline;
+    font-style: italic;
+  }
+`;
+
+const DesktopHeader = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 export default Header;
